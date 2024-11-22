7571abdf1407943017417a4b0b84726699292a31
Yousefipouri <97617019+Yousefipouri@users.noreply.github.com>
2024-11-22 12:03:03 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,10 +12,11 @@ import Register from "./Components/Auth/Register";
 import Login from "./Components/Auth/Login";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://fatemehyousefi.pythonanywhere.com"; // تغییر به دامنه PythonAnywhere
 axios.defaults.headers.post['Content-Type'] = "application/json";
 axios.defaults.headers.post['Accept'] = "application/json";
 axios.defaults.withCredentials = true;
+
 
 function App() {
   return (
